09d48d8f4f8f0f55009df1b23783c4e827809e77
KirVoz <k.a.voz97@gmail.com>
2023-03-27 17:12:37 +0600

    sem1 no hw

---
+++ sem_1/Program.cs
@@ -0,0 +1,52 @@
+﻿//int num = Convert.ToInt32(Console.ReadLine());
+//int square = num*num;
+//Console.WriteLine("Square is: " +square);
+
+// Console.Write("Inter first number ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Inter second number ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if(num1 == num2*num2)
+// {
+//     Console.Write("Yes");
+// }
+// else
+// {
+//     Console.Write("No");
+// }
+
+//Console.Write("Введите номер дня недели ");
+//int dayNumber = Convert.ToInt32(Console.ReadLine());
+//if(dayNumber<=7 && dayNumber>=1)
+//{
+//    if(dayNumber==1)
+//    {
+//        Console.Write("понедельник");
+//    }
+//if(dayNumber==2)
+//    {
+//        Console.Write("вторник");
+//    }
+//if(dayNumber==3)
+//    {
+//        Console.Write("среда");
+//    }
+//}
+//else
+//{
+//    Console.Write("Error");
+//}
+
+//Console.Write("N: ");
+//int N = Convert.ToInt32(Console.ReadLine());
+//int negativeN = N*(-1);
+//while (negativeN <= N)
+//{
+//    Console.Write(negativeN);
+//    negativeN++;
+//}
+
+//Console.Write("введите трехзначное число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int last = num%10;
+//Console.Write(last);
