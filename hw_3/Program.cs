b2a5dc439543f7750f3287ef07a04f15e107344d
KirVoz <k.a.voz97@gmail.com>
2023-04-03 13:18:30 +0600

    hw_3

---
+++ hw_3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Console.WriteLine("Введите пятизначное число: ");
+// int fiveDig = Convert.ToInt32(Console.ReadLine());
+// string res = "";
+// int a1 = fiveDig % 10;
+// int a2 = fiveDig / 10 % 10;
+// int a3 = fiveDig / 100 % 10;
+// int a4 = fiveDig / 1000 % 10;
+// int a5 = fiveDig / 10000;
+// int fiveDigRev = a1 * 10000 + a2 * 1000 + a3 * 100 + a4 * 10 + a5;
+// if (fiveDig == fiveDigRev)
+// {
+//     Console.WriteLine("полинндром");
+// }
+// else
+// {
+//     Console.WriteLine("не полиндром");
+// }
+
+
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// int z2 = Convert.ToInt32(Console.ReadLine());
+// double d = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
+// Console.WriteLine(d);
+
+
+// Console.WriteLine("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// for(int i = 1; i <= N; i++)
+// {
+//     Console.WriteLine($" Куб числа {i} = {i*i*i}");
+// }
+
+
+// Console.WriteLine("num: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++)
+// {
+//     Console.WriteLine($"Куб числа {i} = {i*i*i}");
+// }
